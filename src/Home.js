b05a65ec2f7fdd761489b8494329dab72e1c5153
Luis <lvas248@gmail.com>
2023-07-13 16:05:19 -0400

    add placeholder image to home component, include short intro, add some styling

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,36 @@
+import sasha from './media/sasha.jpeg'
 
 function Home() {
     
     return ( 
         <div 
             id='home'
-            className='h-screen pt-20'
+            className='h-screen flex px-3'
             >
-            Home
+
+            <div
+                className='w-[50vw] h-full border-r-2 pt-[8%]'
+                >
+                    <img 
+                        src={sasha} 
+                        alt='place-holder' 
+                        className='h-[40vh] rounded-full'
+
+                        />
+                 
+
+            </div>
+
+            <div
+                className='flex flex-col w-[50vw] h-full pt-[8%] text-left place-content-center'
+                >
+                    <h3 className='text-lg'>Hello there!</h3>
+                    <h1 className='text-5xl '>I am</h1>
+                    <h1 className='text-7xl font-bold text-[rgb(225,74,56)] underline underline-offset-3'>Luis Vasquez</h1>
+                    <p className='text-lg mt-3'>I am a motivated full stack developer with a strong commitment to creating functional and visually appealing web applications. Currently, I am actively searching for new opportunities to grow and contribute my skills.</p>
+
+            </div>
+
         </div> 
     );
 }
