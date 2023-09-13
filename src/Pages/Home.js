a95746fbfc02f92342f11823189b54fc2ca43d01
Luis <lvas248@gmail.com>
2023-09-13 11:36:37 -0400

    style update for home comp

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
                 className={` rounded-3xl place-self-end border-black border-[6px] border-x-[6px] relative grid bgGreen p-4 
                  mt-[5vh]
                  h-[65vh] sm:h-[35vh] md:h-[40vh] transition-height duration-500 
-                ${expand && ' h-[95vh] sm:h-[54vh] md:h-[55vh] lg:h-[55vh] '}
+                ${expand && ' h-[97vh] sm:h-[54vh] md:h-[60vh] lg:h-[55vh] '}
                 `}
             >
                 <div className='sm:grid sm:grid-cols-2  text-white'>
@@ -31,8 +31,7 @@ function Home() {
                                 bgOrange pt-4 px-4 rounded-t-full border-black border-[6px]
                                 place-self-center
                                 max-h-[296px]
-                                
-'>
+                            '>
                         <img 
                             src={sasha} 
                             alt='place-holder' 
@@ -49,8 +48,8 @@ function Home() {
 
 
 
-                <div className={`p-4 m-auto text-white animate-fade-in  ${expand ?'block': 'hidden'} border-t-1 `}>
-                    <h3>About.</h3>                    
+                <div className={`p-4 m-auto mt-3 text-white animate-fade-in  ${expand ?'block': 'hidden'} border-t-[6px] border-black `}>
+                    <h3 className=''>About.</h3>                    
                     <p className='text-xs sm:text-xs'>As a former Retail Operations Manager, I became well-acquainted with the dynamic world of commerce, where precision and efficiency were the key to my success. My journey extended into the fields of construction and architecture, where I uncovered a genuine passion for building from the ground up. Today, I've shifted my focus to web development, drawing from these diverse experiences to craft websites and online apps firmly rooted in a strong foundation.</p>    
 
                 </div>   
