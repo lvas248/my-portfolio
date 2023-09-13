070d191176d88238920ddfd2cabf21b10563756f
Luis <lvas248@gmail.com>
2023-09-13 11:17:29 -0400

    update layout in home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,43 +12,46 @@ function Home() {
             >
 
             <div
-                className='h-[25vh] textGreen grid place-content-center text-center m-auto w-[80%] '
-            >
-                <h1 className='text-[40px] sm:text-[50px] font-bold my-auto'> Luis Vasquez.</h1>
-
-
-                    <div className='p-2'>
-                        <h2 className='my-auto pl-2 text-2xl'>Web Developer.</h2>
-                        <div className='text-xs pl-4 m-auto'>
-                            <p className='my-auto'>Front end | Back end</p>
-                            <p className='my-auto'> NYC based</p>
-                        </div>
-
-                    </div>
-
-
-            </div>
-
-            <div
                 className={` rounded-3xl place-self-end border-black border-[6px] border-x-[6px] relative grid bgGreen p-4 
-                h-[50vh] transition-all duration-[0.500s] 
-                ${ expand && 'h-[85vh] sm:h-[70vh]'}
+                 mt-[5vh]
+                 h-[60vh] sm:h-[35vh] md:h-[40vh] transition-height duration-500 overflow-hidden
+                ${expand && ' h-[80vh] sm:h-[54vh] md:h-[55vh] lg:h-[55vh] '}
                 `}
             >
-                <img 
-                    src={sasha} 
-                    alt='place-holder' 
-                    className='
-                        animate-scale-in
-                        h-[35vh] bgOrange pt-4 px-4 rounded-t-full border-black border-[6px]
-                        place-self-center
-                    '
-                />
+                <div className='sm:grid sm:grid-cols-2  text-white'>
+
+                    <div className='col-span-1  w-full flex flex-col place-content-center text-center'>
+                        <div className='m-auto '>
+                            <h3 className='font-bold'>Luis Vasquez</h3>
+                            <h1 className='font-bold text-[40px]'>Full Stack Developer</h1>
+                            <p className='text-xs'>I construct end-to-end web applications that are dependable, scalable, and prioritize an exceptional user experience.</p>
+                        </div>
+                    </div>
+                    <div className='                                
+                                bgOrange pt-4 px-4 rounded-t-full border-black border-[6px]
+                                place-self-center
+                                max-h-[296px]
+                                
+'>
+                        <img 
+                            src={sasha} 
+                            alt='place-holder' 
+                            className='
+                                object-cover
+                                mx-auto
+                                animate-scale-in
+                                max-h-[250px]
+                            '
+                        />                   
+
+                    </div>
+                </div>
 
 
-                <div className={`p-4 m-auto text-white animate-fade-in ${!expand && 'hidden'}`}>
+
+                <div className={`p-4 m-auto text-white animate-fade-in  ${expand ?'block': 'hidden'} border-t-1 `}>
                     <h3>About.</h3>                    
-                    <p className='text-xs sm:text-sm'>As a former Retail Operations Manager, I became well-acquainted with the dynamic world of commerce, where precision and efficiency were the key to my success. My journey extended into the fields of construction and architecture, where I uncovered a genuine passion for building from the ground up. Today, I've shifted my focus to web development, drawing from these diverse experiences to craft websites and online apps firmly rooted in a strong foundation.</p>    
+                    <p className='text-xs sm:text-xs'>As a former Retail Operations Manager, I became well-acquainted with the dynamic world of commerce, where precision and efficiency were the key to my success. My journey extended into the fields of construction and architecture, where I uncovered a genuine passion for building from the ground up. Today, I've shifted my focus to web development, drawing from these diverse experiences to craft websites and online apps firmly rooted in a strong foundation.</p>    
 
                 </div>   
 
